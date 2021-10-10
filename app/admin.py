7e2838b1acecd984c669583d67b1d64c9da4b7a7
Timur Bakibayev <timurbakibayev@gmail.com>
2021-10-10 19:26:56 +0200

    Property and Profile

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from app.models import Subscription, Incident
+from app.models import Subscription, Incident, Profile, Property
 
 admin.site.register(Subscription)
 admin.site.register(Incident)
+admin.site.register(Profile)
+admin.site.register(Property)
